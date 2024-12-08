d7f1157ecc005714e2e853bf06ef9daec79fa43d
adisbladis <adisbladis@gmail.com>
2024-12-08 03:36:37 +0000

    flake: Use / syntax for nixpkgs ref
    It seems that renovate not discovering Nix might be related to that.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Pyproject.nix base package set";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     pyproject-nix = {
       url = "github:nix-community/pyproject.nix";
